beec807af821734cdf989571f4e05e87a0d1dd83
Mustafa-tariq23 <mt2032303@gmail.com>
2023-11-15 22:32:18 +0500

    Get a quota improved

--- js/main.js
+++ js/main.js
@@ -70,18 +70,138 @@
 
 
 function sendMail() {
+    // Retrieve form field values
+    var nameValue = document.getElementById("name").value;
+    var emailValue = document.getElementById("email").value;
+    var sendingDateValue = document.getElementById("sendingDate").value;
+    var sendingTimeValue = document.getElementById("sendingTime").value;
+    var messageValue = document.getElementById("Message").value;
+
+    if (!nameValue || !emailValue || !sendingDateValue || !sendingTimeValue || !messageValue) {
+        alert("Please fill in all fields before submitting.");
+        return false; // Prevent form submission
+    }
+
     var Params = {
-        from_name: document.getElementById("name").value,
-        email_id: document.getElementById("email").value,
-        sending_date: document.getElementById("sendingDate").value,
-        sending_time: document.getElementById("sendingTime").value,
-        message: document.getElementById("Message").value,
+        from_name: nameValue,
+        email_id: emailValue,
+        sending_date: sendingDateValue,
+        sending_time: sendingTimeValue,
+        message: messageValue,
     };
 
-    emailjs.send("service_76ws5tl", "template_9udtz05", Params).then(function(res){
+    emailjs.send("service_76ws5tl", "template_9udtz05", Params).then(function(res) {
         alert("Successful! " + res.status);
-    })
+
+        // Reset form fields
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("sendingDate").value = "";
+        document.getElementById("sendingTime").value = "";
+        document.getElementById("Message").value = "";
+    });
+
+    // Prevent form submission
+    return false;
 }
+
+let isButtonDisabled = false; // Flag to track button disable status
+
+// Add an oninput event to each input field to check whether to enable the submit button
+document.getElementById("name").oninput = checkFormFields;
+document.getElementById("email").oninput = checkFormFields;
+document.getElementById("sendingDate").oninput = checkFormFields;
+document.getElementById("sendingTime").oninput = checkFormFields;
+document.getElementById("Message").oninput = checkFormFields;
+
+// Function to check if all fields are filled and enable/disable the submit button accordingly
+function checkFormFields() {
+    var nameValue = document.getElementById("name").value;
+    var emailValue = document.getElementById("email").value;
+    var sendingDateValue = document.getElementById("sendingDate").value;
+    var sendingTimeValue = document.getElementById("sendingTime").value;
+    var messageValue = document.getElementById("Message").value;
+
+    // Enable the submit button only if all fields are filled
+    var submitButton = document.querySelector('#submitButton');
+    submitButton.disabled = !(nameValue && emailValue && sendingDateValue && sendingTimeValue && messageValue && isValidEmail(emailValue));
+}
+
+// Function to validate email format
+function isValidEmail(email) {
+    // Use a regular expression for basic email format validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function sendMail() {
+    // Check if the button is already disabled
+    if (isButtonDisabled) {
+        alert("You have already submitted a request. Please wait for a response.");
+        return false; // Prevent form submission
+    }
+
+    // Retrieve form field values
+    var nameValue = document.getElementById("name").value;
+    var emailValue = document.getElementById("email").value;
+    var sendingDateValue = document.getElementById("sendingDate").value;
+    var sendingTimeValue = document.getElementById("sendingTime").value;
+    var messageValue = document.getElementById("Message").value;
+
+    // Validate email format
+    if (!isValidEmail(emailValue)) {
+        alert("Please enter a valid Gmail address.");
+        return false; // Prevent form submission
+    }
+
+    // Disable the button to prevent multiple submissions
+    document.getElementById("submitButton").disabled = true;
+    isButtonDisabled = true;
+
+    // Prepare parameters for emailjs.send
+    var Params = {
+        from_name: nameValue,
+        email_id: emailValue,
+        sending_date: sendingDateValue,
+        sending_time: sendingTimeValue,
+        message: messageValue,
+    };
+
+    // Send email using emailjs
+    emailjs.send("service_76ws5tl", "template_9udtz05", Params).then(function(res) {
+        alert("Successful! " + res.status);
+
+        // Reset form fields
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("sendingDate").value = "";
+        document.getElementById("sendingTime").value = "";
+        document.getElementById("Message").value = "";
+    });
+
+    // Prevent form submission
+    return false;
+}
+
+
+
+
+
+// function sendMail() {
+//     var Params = {
+//         from_name: document.getElementById("name").value,
+//         email_id: document.getElementById("email").value,
+//         sending_date: document.getElementById("sendingDate").value,
+//         sending_time: document.getElementById("sendingTime").value,
+//         message: document.getElementById("Message").value,
+//     };
+
+//     emailjs.send("service_76ws5tl", "template_9udtz05", Params).then(function(res){
+//         alert("Successful! " + res.status);
+//     })
+
+//     return false;
+// }
 
 // .then(function(res) {
 //     alert("Message sent successfully " + res.status);
